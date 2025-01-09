67e2bd688f6b7287c04f3efd79bc898296173429
Tsageon <sagaetshepo@gmail.com>
2025-01-09 13:13:38 +0200

    UTC10

--- Backend/controllers/TimeZ.js
+++ Backend/controllers/TimeZ.js
@@ -1,11 +1,13 @@
 const timezoneMiddleware = (req, res, next) => {
     const timezoneFromHeader = req.headers['x-timezone'];
-
-    console.log('Timezone from header:', timezoneFromHeader);
-
-    req.timezone = timezoneFromHeader || 'Africa/Johannesburg'; 
-    
+    if (!timezoneFromHeader) {
+        console.log('Timezone header is missing, defaulting to Africa/Johannesburg');
+    } else {
+        console.log('Timezone from header:', timezoneFromHeader);
+    }
+    req.timezone = timezoneFromHeader || 'Africa/Johannesburg';
     next();
 };
+
 
 module.exports = timezoneMiddleware;

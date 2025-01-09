92d8b27a16a4898a3bb886d5aef73abbd7e206fc
Tsageon <sagaetshepo@gmail.com>
2025-01-09 11:24:49 +0200

    UTCTAKE4

--- Backend/Server.js
+++ Backend/Server.js
@@ -3,6 +3,7 @@ const connectDB = require('./config/database.js');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
+const timezoneMiddleware = require('./controllers/TimeZ.js')
 const scheduleReminders = require('./controllers/Scheduler.js')
 const userRoutes = require('./routes/UserR.js')
 const resturantRoutes = require('./routes/ResturantR.js')
@@ -15,6 +16,7 @@ scheduleReminders();
 app.use(bodyParser.json());
 
 app.set('trust proxy', true);
+app.use(timezoneMiddleware);
 app.use('/api/', userRoutes);
 app.use('/api/', resturantRoutes);
 app.use('/', paypalRoutes);

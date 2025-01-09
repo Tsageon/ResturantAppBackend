d7d9a59608b3ae2c3fcba3fafa6069fae18c70e1
Tsageon <sagaetshepo@gmail.com>
2025-01-09 12:22:48 +0200

    utc8.1

--- Backend/controllers/TimeZ.js
+++ Backend/controllers/TimeZ.js
@@ -1,9 +1,10 @@
 const timezoneMiddleware = (req, res, next) => {
     const timezoneFromHeader = req.headers['x-timezone'] || 'America/New_York';
     console.log('Timezone from header:', timezoneFromHeader);
-    console.log("Timezone Middleware: x-timezone =", req.headers['x-timezone'])
+    console.log("Timezone Middleware: x-timezone =", req.headers['x-timezone']);   
     req.timezone = timezoneFromHeader || req.headers['x-timezone'] || 'America/New_York';
     next();
 };
+
 
 module.exports = timezoneMiddleware;

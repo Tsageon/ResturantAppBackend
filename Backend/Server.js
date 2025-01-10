c913e3bb2168e848b0e2ab38f49f5763690fa5f6
Tsageon <sagaetshepo@gmail.com>
2025-01-10 12:31:54 +0200

    Routes conflict

--- Backend/Server.js
+++ Backend/Server.js
@@ -24,17 +24,12 @@ app.use((req, res, next) => {
     console.log(`Client IP: ${req.ip}`);
     next();
 });
+
 app.use((err, req, res, next) => {
     res.status(err.status || 500).json({
         message: err.message || 'An unexpected error occurred',
     });
 });
-
-app.use((req, res, next) => {
-    res.status(404).json({
-      message: 'The route you are trying to access does not exist my G.',
-    });
-  });
   
 const PORT = 4000;
 app.listen(PORT, () => {
